898d66a679ed8538c02f6807c54cc98bbf80ee15
sebawael1 <97540803+sebawael1@users.noreply.github.com>
2024-09-06 15:21:11 +0300

    Add files via upload

---
+++ challenges/Main.java
@@ -0,0 +1,28 @@
+public class Main
+{
+  public static String main(int number)
+  {
+      number = 1;
+    switch (number) { 
+      case 1:  System.out.println("One.");
+      break;
+      case 2:  System.out.println("Two.");  
+      break;
+      case 3:  System.out.println("Three.");
+      break;
+      case 4:  System.out.println("Four.");
+      break;
+      case 5:  System.out.println("Five");
+      break;
+      case 6:  System.out.println("Six");
+      break;
+      case 7:  System.out.println("Seven");
+      break;
+      case 8:  System.out.println("eight");
+      break;
+      case 9:  System.out.println("Nine");
+      break;
+    }
+    return "";
+  }
+}
